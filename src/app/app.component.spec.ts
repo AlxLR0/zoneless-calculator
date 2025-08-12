add748e038bcc08b4d84fa201042b7f9766a1a0e
Alejandro <alex.lopez.rubio2600@gmail.com>
2025-08-12 15:08:49 -0600

    mejorar pruebas y scripts de la calculadora

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,7 +1,11 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
+
+  let fixture = ComponentFixture<AppComponent>;
+  let compiled: HTMLElement;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [AppComponent],
@@ -14,16 +18,33 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'zoneless-calculator' title`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('zoneless-calculator');
+  it('should be 3', () => { // esto es un test de ejemplo
+    // Aquí puedes escribir un test que verifique que 1 + 2 es igual a 3
+    //A= Arrange que es preparar los datos necesarios para el test
+    const num1 = 1;
+    const num2 = 2;
+
+    //A= Act que es ejecutar la acción que queremos probar
+    const result = num1 + num2;
+    
+    //A= Assert que es verificar que el resultado es el esperado
+    expect(result).toBe(3);// Aquí verificamos que el resultado es 3
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, zoneless-calculator');
-  });
+ 
+
+
+
+  // it(`should have the 'zoneless-calculator' title`, () => {
+  //   const fixture = TestBed.createComponent(AppComponent);
+  //   const app = fixture.componentInstance;
+  //   expect(app.title).toEqual('zoneless-calculator');
+  // });
+
+  // it('should render title', () => {
+  //   const fixture = TestBed.createComponent(AppComponent);
+  //   fixture.detectChanges();
+  //   const compiled = fixture.nativeElement as HTMLElement;
+  //   expect(compiled.querySelector('h1')?.textContent).toContain('Hello, zoneless-calculator');
+  // });
 });
